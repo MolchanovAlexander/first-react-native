63698705e6f4bf76247de36b04c7b322fe9ff536
MolchanovAlexander <rybkolhoz@gmail.com>
2023-11-11 15:36:09 +0200

    123

--- components/home/nearby/Nearbyjobs.jsx
+++ components/home/nearby/Nearbyjobs.jsx
@@ -1,12 +1,46 @@
-import React from 'react'
-import { View, Text } from 'react-native'
 
-import styles from './nearbyjobs.style'
+import { View, Text, TouchableOpacity, ActivityIndicator } from 'react-native'
+import { useRouter } from "expo-router";
+import styles from './nearbyjobs.style';
+import { COLORS } from '../../../constants';
+import NearbyJobCard from '../../common/cards/nearby/NearbyJobCard'
+import useFetch from "../../../hook/useFetch";
 
 const Nearbyjobs = () => {
+  const router = useRouter();
+  const { data, isLoading, error } = useFetch('search', {
+    query: "Java developer",
+
+    num_pages: '1',
+  })
+  // data?.map((j) => {
+  //   console.log(j.employer_logo);
+  // })
+  
   return (
-    <View>
-      <Text>Nearbyjobs</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Nearbyjobs</Text>
+        <TouchableOpacity style={styles.header}>
+          <Text style={styles.headerBtn}>Show all</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.cardsContainer}>
+        {isLoading ? (
+          <ActivityIndicator size='large' color={COLORS.primary} />
+        ) : error ? (
+          <Text>Something went wrong</Text>
+        ) : (
+          data?.map((j) => (
+            <NearbyJobCard
+              key={j.job_id}
+              job={j}
+              handleNavigate={()=> router.push(`/job-details/${j.job_id}`)}
+            />
+          ))
+        )}
+
+      </View>
     </View>
   )
 }
---
+++ utils/index.js
@@ -0,0 +1,10 @@
+export const checkImageURL = (url) => {
+    if (!url) return false
+    else {
+         console.log(url);
+        // console.log("zzzitem.employer_logo");
+        const pattern = new RegExp('^https?:\\/\\/.+', 'i');
+        console.log(pattern.test(url));
+        return pattern.test(url);
+    }
+};//\\(png|jpg|svg|jpeg|bmp|gif|webp).$
